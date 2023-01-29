7a6406d3d7d110bd21662480133f4d6920c61d1f
Krystian Ogonowski <tailowskikrystian@gmail.com>
2023-01-29 12:41:51 +0100

    exercise 1.12*

--- part1/exercises 1.6-1.14/src/App.tsx
+++ part1/exercises 1.6-1.14/src/App.tsx
@@ -52,6 +52,44 @@ const StatisticLine = ({ text, value, modifier }: StatisticLineProps) => (
 	</tr>
 );
 
+interface FeedbackProps {
+	setGood: () => void;
+	setNeutral: () => void;
+	setBad: () => void;
+}
+
+const Feedback = ({ setGood, setNeutral, setBad }: FeedbackProps) => (
+	<>
+		<h1>give feedback</h1>
+		<Button text="good" onClick={setGood} />
+		<Button text="neutral" onClick={setNeutral} />
+		<Button text="bad" onClick={setBad} />
+	</>
+);
+
+const Anecdote = () => {
+	const [selected, setSelected] = useState(0);
+
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+		"The only way to go fast, is to go well.",
+	];
+
+	return (
+		<>
+			<h1>Anecdote of the day</h1>
+			<p>{anecdotes[selected]}</p>
+			<Button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))} text="Random anecdote" />
+		</>
+	);
+};
+
 const App = () => {
 	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
@@ -60,11 +98,13 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>give feedback</h1>
-			<Button text="good" onClick={() => setGood(good + 1)} />
-			<Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
-			<Button text="bad" onClick={() => setBad(bad + 1)} />
+			<Feedback
+				setGood={() => setGood(prev => prev + 1)}
+				setNeutral={() => setNeutral(prev => prev + 1)}
+				setBad={() => setBad(prev => prev + 1)}
+			/>
 			<Statistics good={good} neutral={neutral} bad={bad} />
+			<Anecdote />
 		</div>
 	);
 };
